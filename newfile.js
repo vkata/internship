e02ae0f192662f173c21202432ecb88aa96a85da
vkata <katavizi@yahoo.com>
2016-08-18 16:21:40 +0300

    first try
    Vrem o aplicatie (consola) prin care ai o lista de persoane si mai multe
    optiuni.
    
    1) adaugi o noua persoana (exemplu: ion ionescu, m, 23).
    2) sortezi persoanele dupa nume si le afisezi
    3) sortezi persoanele dupa varsta si le afisezi
    4) exit

---
+++ newfile.js
@@ -0,0 +1,84 @@
+var persList = [];
+
+function readPersonInfo() {
+
+}
+
+function mainFunction() {
+
+	var readline = require('readline');
+
+	var rl = readline.createInterface(process.stdin, process.stdout);
+
+	console.log('Options: (1) Add new person, (2) Sort by name, (3) Sort by age, (4) Exit\n ');
+	rl.setPrompt('Answer: ');
+	rl.prompt();
+
+	rl.on('line', function(line) {
+		console.log('Options: (1) Add new person, (2) Sort by name, (3) Sort by age, (4) Exit\n ');
+
+		if (line.length > 5) {
+			var temp = line.split(" ");
+			persList.push({
+				name: temp[0],
+				gender: temp[1],
+				age: parseInt(temp[2])
+			});
+		}
+		if (line == 4) {
+			console.log("Exit.");
+			rl.close();
+		} else if (line == 1) {
+			//adding new person
+			console.log('You chose ' + line + ' to add a new person.\n');
+			console.log('Person\'s information (name, gender, age)');
+			readFromUser();
+		} else if (line == 2) {
+			console.log('You chose  ' + line + ' - sort by name.\n');
+			sorting(persList, 'name');
+			console.log(persList);
+			console.log('Options: (1) Add new person, (2) Sort by name, (3) Sort by age, (4) Exit\n ');
+		} else if (line == 3) {
+			console.log('You chose  ' + line + ' - sort by age.\n');
+			sorting(persList, 'age');
+			console.log(persList);
+			console.log('Options: (1) Add new person, (2) Sort by name, (3) Sort by age, (4) Exit\n ');
+		}
+		rl.prompt();
+	}).on('close', function() {
+		process.exit(0);
+	});
+}
+
+function sorting(persList, sortBy) {
+	console.log("zzzz");
+	persList.sort(function(a, b) {
+		if (a[sortBy] > b[sortBy]) {
+			return 1;
+		}
+		if (a[sortBy] < b[sortBy]) {
+			return -1;
+		}
+		return 0;
+	});
+}
+
+mainFunction();
+
+function readFromUser() {
+	var readline = require('readline');
+
+	var rl2 = readline.createInterface(process.stdin, process.stdout);
+
+	// console.log('Options: (1) Add new person, (2) Sort by name, (3) Sort by age, (4) Exit\n ');
+	rl2.setPrompt('zzzzz: ');
+	rl2.prompt();
+
+	rl2.on('line', function(line) {
+		// console.log('Options: (1) Add new person, (2) Sort by name, (3) Sort by age, (4) Exit\n ');
+
+		rl2.prompt();
+	}).on('close', function() {
+		process.exit(0);
+	});
+}
